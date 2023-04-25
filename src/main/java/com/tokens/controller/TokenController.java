e1bc622ee690da16632ccce3ae086f8fee1d1686
amitongraph2023 <129284775+amitongraph2023@users.noreply.github.com>
2023-04-25 18:33:33 +0530

    Update TokenController.java

--- src/main/java/com/tokens/controller/TokenController.java
+++ src/main/java/com/tokens/controller/TokenController.java
@@ -1,5 +1,7 @@
 package com.tokens.controller;
 
+
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.tokens.models.Transaction;
+import com.tokens.models.TransactionStatusLogs;
 import com.tokens.request.CloudRequest;
 import com.tokens.request.TransactionStatusRequest;
 import com.tokens.response.CloudResponse;
@@ -34,6 +37,16 @@ public class TokenController {
 		return ResponseEntity.badRequest().body(new CloudResponse());
 	}
 
+	@GetMapping("/getLogsTransactionToken")
+	public ResponseEntity<List<Transaction>> logsTransactionToken(){
+		List<Transaction> transactionTokenLog = transactionService.logsTransactionToken();
+		if (transactionTokenLog != null) {
+			return ResponseEntity.ok().body(transactionTokenLog);
+		}
+		return ResponseEntity.badRequest().body(transactionTokenLog);
+	}
+	
+	
 	@PostMapping("/updateStatus")
     public ResponseEntity<String> updateTransactionStatus(@RequestBody TransactionStatusRequest request) {
         boolean updated = transactionService.updateTransactionStatus(Integer.parseInt(request.getTransactionId()), request.getStatus());
@@ -45,11 +58,14 @@ public class TokenController {
     }
 	
 	@GetMapping("/getLogsUpdatedStatus")
-	public ResponseEntity<List<Transaction>> logsUpdatedTransactionStatus(){
-		List<Transaction> transaction = transactionService.logsUpdatedTransactionStatus();
-		if (transaction != null) {
-			return ResponseEntity.ok().body(transaction);
+	public ResponseEntity<List<TransactionStatusLogs>> logsUpdatedTransactionStatus(){
+		List<TransactionStatusLogs> transactionLog = transactionService.logsUpdatedTransactionStatus();
+		if (transactionLog != null) {
+			return ResponseEntity.ok().body(transactionLog);
 		}
-		return ResponseEntity.badRequest().body(transaction);
+		return ResponseEntity.badRequest().body(transactionLog);
 	}   
+	
+	
+	
 }
